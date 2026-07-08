ed2038539475e1a6060fd20fba07a26c4897beff
mohd gulfam <mg5708445@gmail.com>
2026-07-08 13:16:02 +0530

    day2 of practice

---
+++ practice/src/GRAPH/numberofprovinces_547.java
@@ -0,0 +1,48 @@
+package GRAPH;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class numberofprovinces_547 {
+    public static int findCircleNum(int[][] adj) {
+        int n = adj.length;
+        int count = 0;
+        boolean[] visited = new boolean[n];
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                bfs(i , visited , adj);
+                count++;
+            }
+        }
+        return count;
+
+    }
+    public static void bfs(int i, boolean visited[], int[][] adj){
+        int n = adj.length;
+        visited[i] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(i);
+        while(!queue.isEmpty()){
+            int front = queue.remove();
+            for(int j=0; j<n; j++){
+                if(adj[front][j] == 1 && visited[j] == false){
+                    queue.add(j);
+                    visited[j] = true;
+                }
+            }
+        }
+
+
+    }
+    public static void main(String[] args) {
+        int[][] adj = {
+                {1,1,0},
+                {1,1,0},
+                {0,0,1}
+        };
+
+
+        System.out.println(findCircleNum(adj));
+
+    }
+}
