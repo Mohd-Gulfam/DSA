7d1b74b53695b91a5f2dfe970e06124565ab7ca7
mohd gulfam <mg5708445@gmail.com>
2026-07-18 17:32:32 +0530

    day 5 of practice

--- practice/src/ARRAY/subArray.java
+++ practice/src/ARRAY/subArray.java
@@ -18,8 +18,31 @@ public class subArray {
         System.out.println("total subarray length:"+ts);
     }
 
+    public static void maxSubArray1(int[] arr){//o(n3)
+        int n = arr.length;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i =0; i<n; i++){
+            int start = i;
+                for(int j=i+1;j<n;j++){
+                    int end = j;
+                    int currSum = 0;
+                    for(int k = start; k<=end; k++){
+                        currSum += arr[k];
+                    }
+                    System.out.println(currSum);
+                    if(maxSum<currSum){
+                        maxSum = currSum;
+                    }
+                }
+        }
+        System.out.println("max subarray sum:"+maxSum);
+
+    }
+
+
     public static void main(String[] args) {
         int arr[] = {2,4,6,7,9,10};
-        printSubArray(arr);
+//        printSubArray(arr);
+           maxSubArray1(arr);
     }
 }
