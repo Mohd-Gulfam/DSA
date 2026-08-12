a8b2731dc0064b0b45b55411d1911214aa7c975e
mohd gulfam <mg5708445@gmail.com>
2026-08-12 22:56:17 +0530

    day 25 of practice

---
+++ practice/src/patterns/practice.java
@@ -0,0 +1,42 @@
+package patterns;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class practice {
+    public static void main(String[] args) {
+        int i=1;
+        int j =1;
+        int k = i++  +  ++i + i-- + --i;
+        boolean b = i++ == 1 && ++j ==2  &&  i++ ==2;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(b);
+        int n=20;
+        int sum =0;
+        for(int i=1;i<=n;i++){
+            if(i == i/2*2){
+                System.out.println("even +"+i);
+                sum+=i;
+            }else {
+                System.out.println("odd -"+i);
+                sum-=i;
+            }
+        }
+        System.out.println(sum);
+
+
+
+        int a = 5;
+        int b = 4;
+//       xor 1^1 = 0 , 1^0 = 0
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
