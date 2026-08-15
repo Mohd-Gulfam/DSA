2066bc65cd31d7e858916e8b1fa09d29e079851f
mohd gulfam <mg5708445@gmail.com>
2026-08-15 23:48:38 +0530

    day 16 of practice

--- practice/src/SumIntgerValue.java
+++ practice/src/SumIntgerValue.java
@@ -1,8 +1,43 @@
+//public class SumIntgerValue {
+//    public static int singleDigit(int n){
+//        int m =  893478;
+//        int c = 1+(n-1) %9;
+//        System.out.println(c);
+//    }
+//
+//    public static int sum(int nums) {
+//        int sum = 0;
+//
+//        while (nums != 0) {
+//            int digit = nums % 10;
+//            sum = sum + digit;
+//            nums = nums / 10;
+//        }
+//
+//        return sum;
+//    }
+//
+//    public static void main(String[] args) {
+//
+//        int nums = 893478;
+//
+//        int sum1 = sum(nums);
+//
+//        while (sum1 > 9) {
+//            sum1 = sum(sum1);
+//        }
+//
+//        System.out.println(sum1);
+//    }
+//}
 public class SumIntgerValue {
-    public static int singleDigit(int n){
-        int n =  893478;
-        int c = 1+(n-1) %9;
-        System.out.println(c);
+
+    public static int singleDigit(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        return 1 + (n - 1) % 9;
     }
 
     public static int sum(int nums) {
@@ -21,12 +56,9 @@ public class SumIntgerValue {
 
         int nums = 893478;
 
-        int sum1 = sum(nums);
+        int result = singleDigit(nums);
 
-        while (sum1 > 9) {
-            sum1 = sum(sum1);
-        }
-
-        System.out.println(sum1);
+        System.out.println(result);
     }
 }
+
