f57815d86d3a2366d4f6112e62e7c20e44431e8f
mohd gulfam <mg5708445@gmail.com>
2026-08-21 23:36:50 +0530

    day 30 of practice

---
+++ practice/src/patterns/ABPramid.java
@@ -0,0 +1,23 @@
+package patterns;
+
+public class ABPramid {
+    public static void main(String[] args) {
+        int n = 5;
+       for(int i=1; i<=n; i++){
+           for(int j=1; j<=n-i; j++){
+               System.out.print("  ");
+           }
+           for(int j = 1; j<=2*i-1; j++ ){
+               if(j%2==1){
+                   System.out.print("A");
+
+               }else {
+                   System.out.print("B");
+               }
+
+           }
+           System.out.println();
+
+       }
+    }
+}
---
+++ practice/src/patterns/AaBbPattern.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class AaBbPattern {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 5; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print((char) ('A' + i) + " ");
+                } else {
+                    System.out.print((char) ('a' + i) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
